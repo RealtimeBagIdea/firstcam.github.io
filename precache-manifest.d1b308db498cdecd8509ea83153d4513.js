2cd390d5387ac743acb1e1ddbea05db9a01211f5
realtime <nonkung8899@hotmail.com>
2020-04-05 03:58:39 +0700

    INit

---
+++ precache-manifest.d1b308db498cdecd8509ea83153d4513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19408c4d9cb67b382d2e683399f2dfbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d9e44c2a52ef9f1fbb2",
+    "url": "/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "3492cae6321ea44a458c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d9e44c2a52ef9f1fbb2",
+    "url": "/static/js/2.168cb08e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.168cb08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3492cae6321ea44a458c",
+    "url": "/static/js/main.7a50b156.chunk.js"
+  },
+  {
+    "revision": "bcafa31ff160c5099c62",
+    "url": "/static/js/runtime-main.db62ccdb.js"
+  }
+]);
